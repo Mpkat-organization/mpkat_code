d610cd0062f9e32917b7f2114226592987da9650
Saravanan Ravi <saravananrs333@gmail.com>
2025-05-20 18:56:55 +0530

    20-05-2025

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 // ----- HERO - Typed JS -----
 document.addEventListener('DOMContentLoaded', function () {
     var typed = new Typed('#typed-text', {
-        strings: ["WELCOME TO MPKAT", "Data Entry Agency", "Transportation", "Real Estate Agency", "Digital Marketing", "Web Development"],
+        strings: [
+            "WELCOME TO MPKAT",
+            "Data Entry Agency",
+            "Digital Marketing",
+            "Web Development"],
         typeSpeed: 50,
         backSpeed: 30,
         backDelay: 1000,
